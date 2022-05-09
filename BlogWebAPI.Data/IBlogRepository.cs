79cf7b66dba0e145f506ef56ed5de03105d6c861
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-09 23:59:27 +0300

    Started setting up a repository pattern for interacting with the data (CRUD like operations). I am using LINQ (Expressions).

---
+++ BlogWebAPI.Data/IBlogRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using BlogWebAPI.Data.Models;
+using BlogWebAPI.Models;
+
+namespace BlogWebAPI.Data;
+
+public interface IBlogRepository<T> where T: EntityModel
+{
+    //Create
+    Task<Guid> Create(T entity);
+    
+    // Read
+    Task<T> GetById(Guid id);
+    Task<T> GetFirstWhere<TOrder>(Expression<Func<T, bool>> whereExp, Expression<Func<T, TOrder>> orderByExp);
+    Task<PaginationResult<T>> GetAll(int page, int perPage);
+    Task<PaginationResult<T>> GetAll(int page, int perPage, Expression<Func<T, bool>> orderByExp);
+    
+    // Update
+    Task<T> Update(T entity);
+    
+    // Delete
+    Task<bool> Delete(Guid id);
+}
---
+++ BlogWebAPI.Models/PaginationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogWebAPI.Models;
+
+public class PaginationResult<T>
+{
+    public long TotalCount { get; set; }
+    public List<T> Results { get; set; }
+    public int ResultsPerPage { get; set; }
+    public long PageNumber { get; set; }
+}
