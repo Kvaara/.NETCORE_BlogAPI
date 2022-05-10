88645bded866c885a0ff1c3ca5d4da16481aada5
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-10 04:14:15 +0300

    Finished the BlogRepository interface and started implementing the concrete BlogRepository from that interface.

--- BlogWebAPI.Data/IBlogRepository.cs
+++ BlogWebAPI.Data/IBlogRepository.cs
@@ -11,7 +11,14 @@ public interface IBlogRepository<T> where T: EntityModel
     
     // Read
     Task<T> GetById(Guid id);
-    Task<T> GetFirstWhere<TOrder>(Expression<Func<T, bool>> whereExp, Expression<Func<T, TOrder>> orderByExp);
+    Task<T> GetFirstWhere<TOrder>(
+        Expression<Func<T, bool>> whereExp, 
+        Expression<Func<T, TOrder>> orderByExp);
+
+    Task<List<T>> GetAllWhere<TOrder>(
+        Expression<Func<T, bool>> whereExp,
+        Expression<Func<T, TOrder>> orderByExp,
+        int limit = 1000);
     Task<PaginationResult<T>> GetAll(int page, int perPage);
     Task<PaginationResult<T>> GetAll(int page, int perPage, Expression<Func<T, bool>> orderByExp);
     
