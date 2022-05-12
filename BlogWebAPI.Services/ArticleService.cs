5e7c10154200841f794aebae3eb514b2d3dbb989
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-12 23:32:24 +0300

    Started finishing up the ArticleController by adding HTTP endpoints for the client/frontend to access.

--- BlogWebAPI.Services/ArticleService.cs
+++ BlogWebAPI.Services/ArticleService.cs
@@ -1,6 +1,33 @@
-﻿namespace BlogWebAPI.Services;
+﻿using BlogWebAPI.Models;
+using BlogWebAPI.Services.Interfaces;
+using BlogWebAPI.Services.Models;
 
-public class ArticleService
+namespace BlogWebAPI.Services;
+
+public class ArticleService: IArticleService
 {
-    
+    public Task<PagedServiceResult<ArticleDto>> GetAll(int page, int perPage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<ArticleDto>> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<ArticleDto>> Update(Guid id, ArticleDto article)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<Guid>> Create(ArticleDto article)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<Guid>> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- BlogWebAPI.Web/Controllers/ArticleController.cs
+++ BlogWebAPI.Web/Controllers/ArticleController.cs
@@ -1,3 +1,5 @@
+using BlogWebAPI.Models;
+using BlogWebAPI.Services.Interfaces;
 using BlogWebAPI.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,14 +20,53 @@ public class ArticleController : ControllerBase
         _articleService = articleService;
     }
 
+    [HttpPost("/article")]
+    public async Task<ActionResult> CreateArticle([FromBody] ArticleDto article)
+    {
+        _logger.LogDebug($"Creating a new article: {article.Title}");
+        var newArticle = await _articleService.Create(article);
+
+        if (newArticle.Error is not null)
+        {
+            _logger.LogError($"Service level error creating  an Article: {newArticle.Error}");
+            return StatusCode(StatusCodes.Status500InternalServerError, "There was an error creating the Article.");
+        }
+        
+        _logger.LogDebug($"Created an Article: {article.Title} ({newArticle.Data})");
+        return StatusCode(StatusCodes.Status201Created, new {id = newArticle.Data});
+    }
+
+    [HttpGet("/article/{id}")]
+    public async Task<ActionResult> GetArticle(string id)
+    {
+        try
+        {
+        var guid = Guid.Parse(id);
+        var article = await _articleService.GetById(guid);
+        }
+        catch (FormatException err)
+        {
+            _logger.LogWarning($"There was a GUID related formatting error for article: {id}");
+            return BadRequest(id);
+        }
+    }
+
     [HttpGet("/article")]
     public async Task<ActionResult> GetPaginatedArticles([FromQuery] ManyArticlesRequest query)
     {
         // 1 and 3 are so called "magic numbers".
-        // They should be written more cleanly and transferred to static variables that are more understandable.
+        // They should be written more cleanly and transferred to static variables that are more understandable/readable.
         var page = query.Page == 0 ? 1 : query.Page;
         var perPage = query.PerPage == 0 ? 3 : query.PerPage;
         var articles = await _articleService.GetAll(page, perPage);
-        // return Ok("Hello!");
+
+        if (articles.Error is not null)
+        {
+            _logger.LogError($"Error retrieving paginated articles: {articles.Error}");
+            return StatusCode(StatusCodes.Status500InternalServerError, "There was an error retrieving articles");
+        }
+        
+        _logger.LogDebug($"Retrieved Articles total: {articles.Data.TotalCount}");
+        return Ok(articles.Data);
     }
 }
