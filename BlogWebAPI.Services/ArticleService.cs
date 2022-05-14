f3ffc8922fbb15538ef52d869d4e6b84025f7669
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-14 21:42:27 +0300

    Started implementing the ArticleService. This is where I am going to use AutoMapper to automatically map (without having to write additional code) the Article entities into the data object models that we created.

--- BlogWebAPI.Services/ArticleService.cs
+++ BlogWebAPI.Services/ArticleService.cs
@@ -1,14 +1,67 @@
-﻿using BlogWebAPI.Models;
+﻿using AutoMapper;
+using BlogWebAPI.Data;
+using BlogWebAPI.Data.Models;
+using BlogWebAPI.Models;
 using BlogWebAPI.Services.Interfaces;
 using BlogWebAPI.Services.Models;
+using Microsoft.Extensions.Logging;
 
 namespace BlogWebAPI.Services;
 
 public class ArticleService: IArticleService
 {
-    public Task<PagedServiceResult<ArticleDto>> GetAll(int page, int perPage)
+    private readonly IBlogRepository<Article> _articlesRepo;
+    private readonly IBlogRepository<ArticleTag> _articleTags;
+    private readonly IBlogRepository<Tag> _tags;
+    private readonly IBlogRepository<User> _users;
+    private readonly ILogger<ArticleService> _logger;
+    private readonly IMapper _mapper;
+    public ArticleService(
+        IBlogRepository<Article> articlesRepo,
+        IBlogRepository<ArticleTag> articleTags,
+        IBlogRepository<Tag> tags,
+        IBlogRepository<User> users,
+        ILogger<ArticleService> logger,
+        IMapper mapper
+    )
     {
-        throw new NotImplementedException();
+        _articlesRepo = articlesRepo;
+        _articleTags = articleTags;
+        _tags = tags;
+        _users = users;
+        _logger = logger;
+        _mapper = mapper;
+    }
+    
+    public async Task<PagedServiceResult<ArticleDto>> GetAll(int page, int perPage)
+    {
+        var articles = await _articlesRepo.GetAll(page, perPage);
+
+        var articleModels = new List<ArticleDto>();
+        
+        // Adding the corresponding tags and users to the Article data transfer objects
+        foreach (var article in articles.Results)
+        {
+            var articleModel = _mapper.Map<ArticleDto>(article);
+            
+            // Getting all of the associated Article tags:
+            var articleTags = await _articleTags.GetAllWhere();
+        }
+        
+        var articlesResultModel = new PaginationResult<ArticleDto>
+        {
+            TotalCount = 0,
+            Results = null,
+            ResultsPerPage = 0,
+            PageNumber = null,
+        }
+        
+        return new PagedServiceResult<ArticleDto>
+        {
+            IsSuccess = true,
+            Data = articlesResultModel,
+            Error = null,
+        }
     }
 
     public Task<ServiceResult<ArticleDto>> GetById(Guid id)
