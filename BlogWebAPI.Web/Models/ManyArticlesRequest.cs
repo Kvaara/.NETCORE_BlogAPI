8ee430bd5f08c7008e91d4adf3f037b579679534
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-17 23:28:23 +0300

    Made the ManyArticlesRequest Tags property nullable

--- BlogWebAPI.Web/Models/ManyArticlesRequest.cs
+++ BlogWebAPI.Web/Models/ManyArticlesRequest.cs
@@ -5,5 +5,5 @@ public class ManyArticlesRequest
 {
     public int Page { get; set; }
     public int PerPage { get; set; }
-    public List<string> Tags { get; set; }
+    public List<string>? Tags { get; set; }
 }
