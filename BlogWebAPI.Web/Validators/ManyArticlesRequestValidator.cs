635a06a5478445bc4ec27a7c3f284a982e0a900f
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-18 21:44:21 +0300

    Implemented FluentValidation logic into the ManyArticlesRequest. We can now specify the acceptable queries/bodies for different requests.

---
+++ BlogWebAPI.Web/Validators/ManyArticlesRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using BlogWebAPI.Web.Models;
+using FluentValidation;
+
+namespace BlogWebAPI.Web.Validators;
+
+/// <summary>
+/// Validates a "Many Articles" request schema.
+/// </summary>
+public class ManyArticlesRequestValidator: AbstractValidator<ManyArticlesRequest>
+{
+    public ManyArticlesRequestValidator()
+    {
+        RuleFor(x => x.Page)
+            .GreaterThan(0)
+            .When(x => x.Page != 0)
+            .WithMessage("Page must be a positive integer.");
+
+        RuleFor(x => x.PerPage)
+            .GreaterThan(0)
+            .When(x => x.PerPage != 0)
+            .LessThan(101)
+            .When(x => x.PerPage != 0)
+            .WithMessage("Must be an integer between 1 and 100");
+    }
+}
