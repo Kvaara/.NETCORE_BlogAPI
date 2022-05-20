3b81a4f5d2a81fb1b680219dc7f60530e1866b8c
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-20 14:39:53 +0300

    Added FluentValidation as services. Setup the Data Validators and added them as transient services for the web API.

--- BlogWebAPI.Web/Program.cs
+++ BlogWebAPI.Web/Program.cs
@@ -1,8 +1,12 @@
 using BlogWebAPI.Data;
+using BlogWebAPI.Models;
 using BlogWebAPI.Services;
 using BlogWebAPI.Services.Interfaces;
 using BlogWebAPI.Services.Serialization;
 using BlogWebAPI.Web.Middleware;
+using BlogWebAPI.Web.Models;
+using BlogWebAPI.Web.Validators;
+using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Serialization;
@@ -19,7 +23,7 @@ builder.Services
         {
             NamingStrategy = new CamelCaseNamingStrategy()
         }
-    );
+    ).AddFluentValidation();
 
 builder.Services.AddAutoMapper(
     config => config.AddProfile<EntityMappingProfile>(), 
@@ -30,6 +34,18 @@ builder.Services.AddScoped(typeof(IBlogRepository<>), typeof(BlogRepository<>));
 
 // Service Layer
 builder.Services.AddTransient<IArticleService, ArticleService>();
+
+// Data Validators
+builder.Services.AddTransient<AbstractValidator<ArticleDto>, ArticleValidator>();
+builder.Services.AddTransient<AbstractValidator<TagDto>, TagValidator>();
+builder.Services.AddTransient<AbstractValidator<CommentDto>, CommentValidator>();
+builder.Services.AddTransient<AbstractValidator<UserDto>, UserValidator>();
+
+builder.Services.AddTransient<AbstractValidator<ManyArticlesRequest>, ManyArticlesRequestValidator>();
+builder.Services.AddTransient<AbstractValidator<ManyCommentsRequest>, ManyCommentsRequestValidator>();
+builder.Services.AddTransient<AbstractValidator<ManyTagsRequest>, ManyTagsRequestValidator>();
+builder.Services.AddTransient<AbstractValidator<ManyUsersRequest>, ManyUsersRequestValidator>();
+
 
 // You can also add classes using the Singleton pattern
 
