c81fb248c0253340e871980eb645a1cec332d40e
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-22 12:48:07 +0300

    Added the other project directories as references into the Tests project. Started creating the ArticleServiceTest class, which will be used to conduct various tests against our implementations.

--- BlogWebAPI.Tests/ArticleServiceTest.cs
+++ BlogWebAPI.Tests/ArticleServiceTest.cs
@@ -1,12 +1,50 @@
+using AutoMapper;
+using BlogWebAPI.Data;
+using BlogWebAPI.Data.Models;
+using BlogWebAPI.Models;
+using BlogWebAPI.Services;
+using BlogWebAPI.Tests.Helpers;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace BlogWebAPI.Tests;
 
 public class ArticleServiceTest
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "Invoke the article repository once to return a PagedServiceResult when GetAll is called.")]
+    public void Invokes_article_repository_to_return_paged_service_result_for_GetAll()
     {
+        var articleRepoMock = new Mock<IBlogRepository<Article>>();
+        var articleTagRepoMock = new Mock<IBlogRepository<ArticleTag>>();
+        var tagRepoMock = new Mock<IBlogRepository<Tag>>();
+        var userRepoMock = new Mock<IBlogRepository<User>>();
+        var logMock = new Mock<ILogger<ArticleService>>();
+        var mapperMock = new Mock<IMapper>();
 
+        // Generate some fake data (using Bogus) for the mock to return:
+        var fakeArticles = FakeBlogEntityFactory.GenerateFakeArticles(3);
+        const int perPage = 20;
+        const int pageNumber = 1;
+
+        articleRepoMock.Setup(s => s.GetAll(
+            It.IsAny<int>(), 
+            It.IsAny<int>()
+            )
+        ).ReturnsAsync(new PaginationResult<Article>
+        {
+            PageNumber = pageNumber,
+            ResultsPerPage = perPage,
+            Results = fakeArticles,
+        });
+        
+        var sut = new ArticleService(
+            articleRepoMock.Object,
+            articleTagRepoMock.Object,
+            tagRepoMock.Object,
+            userRepoMock.Object,
+            logMock.Object,
+            mapperMock.Object
+            );
     }
 }
---
+++ BlogWebAPI.Tests/Helpers/FakeBlogEntityFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using BlogWebAPI.Data.Models;
+
+namespace BlogWebAPI.Tests.Helpers;
+
+public static class FakeBlogEntityFactory
+{
+    public static Article GenerateFakeArticle()
+    {
+        return new Article();
+    }
+
+    public static List<Article> GenerateFakeArticles(int number)
+    {
+        return new List<Article>();
+    }
+}
