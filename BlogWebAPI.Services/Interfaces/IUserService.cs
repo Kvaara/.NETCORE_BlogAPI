1ffc6703d5f0234c7c7e32686b98ec10635ef93d
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-24 22:28:40 +0300

    Created a new interface called IUserService.

---
+++ BlogWebAPI.Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using BlogWebAPI.Models;
+using BlogWebAPI.Services.Models;
+
+namespace BlogWebAPI.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<PagedServiceResult<UserDto>> GetAll(int page, int perPage);
+    Task<ServiceResult<UserDto>> GetById(Guid id);
+    Task<ServiceResult<UserDto>> Update(Guid id, UserDto userDto);
+    Task<ServiceResult<Guid>> Create(UserDto userDto);
+    Task<ServiceResult<Guid>> Delete(Guid id);
+}
