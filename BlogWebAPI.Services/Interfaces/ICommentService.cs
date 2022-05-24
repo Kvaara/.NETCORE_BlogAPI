846afc43e7fb3ed9cd51711d99a34606f353c318
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-24 22:25:13 +0300

    Created a new interface called ITagService.

--- BlogWebAPI.Services/Interfaces/ICommentService.cs
+++ BlogWebAPI.Services/Interfaces/ICommentService.cs
@@ -1,6 +1,13 @@
-﻿namespace BlogWebAPI.Services.Interfaces;
+﻿using BlogWebAPI.Models;
+using BlogWebAPI.Services.Models;
+
+namespace BlogWebAPI.Services.Interfaces;
 
 public interface ICommentService
 {
-    
+    Task<PagedServiceResult<CommentDto>> GetAll(int page, int perPage, Guid? articleId = null);
+    Task<ServiceResult<CommentDto>> GetById(Guid id);
+    Task<ServiceResult<CommentDto>> Update(Guid id, CommentDto commentDto);
+    Task<ServiceResult<Guid>> Create(CommentDto commentDto);
+    Task<ServiceResult<Guid>> Delete(Guid id);
 }
---
+++ BlogWebAPI.Services/Interfaces/ITagService.cs
@@ -0,0 +1,13 @@
+﻿using BlogWebAPI.Models;
+using BlogWebAPI.Services.Models;
+
+namespace BlogWebAPI.Services.Interfaces;
+
+public interface ITagService
+{
+    Task<PagedServiceResult<TagDto>> GetAll(int page, int perPage);
+    Task<ServiceResult<TagDto>> GetById(Guid id);
+    Task<ServiceResult<TagDto>> Update(Guid id, TagDto tagDto);
+    Task<ServiceResult<Guid>> Create(TagDto tagDto);
+    Task<ServiceResult<Guid>> Delete(Guid id);
+}
