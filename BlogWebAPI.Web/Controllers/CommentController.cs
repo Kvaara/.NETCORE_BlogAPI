8f373ddbbb0ad53f6e91b87ae4b626eb291da030
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-05-25 23:43:39 +0300

    Started creating a controller for a Comment.

---
+++ BlogWebAPI.Web/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BlogWebAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebAPI.Web.Controllers;
+
+/// <summary>
+/// Handles HTTP-requests related to Comments.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class CommentController : ControllerBase
+{
+    private readonly ILogger<CommentController> _logger;
+    private readonly ICommentService _commentService;
+
+    public CommentController(
+        ILogger<CommentController> logger,
+        ICommentService commentService)
+    {
+        _logger = logger;
+        _commentService = commentService;
+    }
+    
+    
+    
+}
