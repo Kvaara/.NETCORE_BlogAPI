5e0387f2229985f29ddfc5a135be817792fbf325
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-06-01 19:05:56 +0300

    Completed a POST endpoint in the TagController for the client to create a new tag.

--- BlogWebAPI.Web/Controllers/TagController.cs
+++ BlogWebAPI.Web/Controllers/TagController.cs
@@ -1,4 +1,5 @@
-﻿using BlogWebAPI.Services.Interfaces;
+﻿using BlogWebAPI.Models;
+using BlogWebAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogWebAPI.Web.Controllers;
@@ -19,5 +20,32 @@ public class TagController : ControllerBase
     {
         _logger = logger;
         _tagService = tagService;
+    }
+
+    /// <summary>
+    /// Handles a POST request for creating a Tag.
+    /// </summary>
+    /// <param name="tagDto"></param>
+    /// <returns></returns>
+    [HttpPost("/tag")]
+    public async Task<ActionResult> CreateTag([FromBody] TagDto tagDto)
+    {
+        _logger.LogDebug($"Creating a new Tag: {tagDto.Name}");
+        var newTag = await _tagService.Create(tagDto);
+
+        if (newTag.Error != null && newTag.Error.Message == "TAG_EXISTS")
+        {
+            _logger.LogWarning($"Service level warning: attempted tto create a duplicate Tag: {newTag.Error}");
+            return StatusCode(StatusCodes.Status409Conflict, $"The Tag {tagDto.Name} already exists.");
+        }
+
+        if (newTag.Error != null)
+        {
+            _logger.LogError($"Service level error creating a Tag: {newTag.Error}");
+            return StatusCode(StatusCodes.Status500InternalServerError, "There was an error creating a Tag");
+        }
+        
+        _logger.LogDebug($"Created a Tag: {tagDto.Name} ({newTag.Data})");
+        return StatusCode(StatusCodes.Status201Created, new {id = newTag.Data});
     }
 }
